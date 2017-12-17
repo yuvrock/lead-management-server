88a902eb01a862fa124f63f3ed9cccb492ccaf5d
Zhao Wei Liew <zhaoweiliew@gmail.com>
2017-12-18 02:29:27 +0800

    Add a vertical navigation bar via angular-patternfly (#4)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 // serve static content from under /public/ dir
 app.use('/', express.static(__dirname + "/public/"));
 
+// Serve some script & stylesheet dependencies from angular-patternfly
+app.use('/dependencies', express.static(__dirname + "/node_modules/angular-patternfly/"));
 
 
 app.listen(4000, function(){
